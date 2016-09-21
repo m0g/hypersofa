fb750496c257e48aa2e4d912670e931790f41e2b
Loïc Nogues <nogues.loic@gmail.com>
2016-09-21 16:16:27 +0200

    Small fixes

--- src/browser.js
+++ src/browser.js
@@ -10,10 +10,10 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 1200, height: 700})
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/../static/index.html`)
+  mainWindow.loadURL(`file://${__dirname}/../static/index.html`);
 
   // Open the DevTools.
   //mainWindow.webContents.openDevTools()
